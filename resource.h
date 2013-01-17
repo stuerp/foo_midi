e3d41f4fba158a9f7a4073b638610b8978d59903
Chris Moeller <kode54@gmail.com>
2013-01-16 19:03:10 -0800

    Rearranged the preferences page

--- resource.h
+++ resource.h
@@ -24,9 +24,10 @@
 #define IDC_CACHED                      1018
 #define IDC_CACHED_TEXT                 1019
 #define IDC_ADL_BANK                    1020
-#define IDC_ADL_CHIPS                   1021
-#define IDC_CHECK1                      1022
-#define IDC_ADL_PANNING                 1022
+#define IDC_ADL_BANK_TEXT               1021
+#define IDC_ADL_CHIPS                   1022
+#define IDC_ADL_CHIPS_TEXT              1023
+#define IDC_ADL_PANNING                 1024
 
 // Next default values for new objects
 // 
@@ -34,7 +35,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
